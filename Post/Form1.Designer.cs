1bbc142036e995dd3282dcd86b353adb7d5362cf
cchen2004 <cchen2004@gmail.com>
2018-03-24 14:54:41 -0400

    Initial commit

---
+++ Post/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Post
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TbUploadFile = new System.Windows.Forms.TextBox();
+            this.BtnBrowse = new System.Windows.Forms.Button();
+            this.BtnOutput = new System.Windows.Forms.Button();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.cbDetailedMode = new System.Windows.Forms.CheckBox();
+            this.Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Panel1
+            // 
+            this.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Panel1.Controls.Add(this.cbDetailedMode);
+            this.Panel1.Controls.Add(this.label3);
+            this.Panel1.Controls.Add(this.label2);
+            this.Panel1.Controls.Add(this.label1);
+            this.Panel1.Controls.Add(this.TbUploadFile);
+            this.Panel1.Controls.Add(this.BtnBrowse);
+            this.Panel1.Controls.Add(this.BtnOutput);
+            this.Panel1.Location = new System.Drawing.Point(126, 80);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(703, 220);
+            this.Panel1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // TbUploadFile
+            // 
+            this.TbUploadFile.Location = new System.Drawing.Point(36, 39);
+            this.TbUploadFile.Name = "TbUploadFile";
+            this.TbUploadFile.Size = new System.Drawing.Size(439, 20);
+            this.TbUploadFile.TabIndex = 1;
+            // 
+            // BtnBrowse
+            // 
+            this.BtnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnBrowse.Location = new System.Drawing.Point(481, 35);
+            this.BtnBrowse.Name = "BtnBrowse";
+            this.BtnBrowse.Size = new System.Drawing.Size(75, 31);
+            this.BtnBrowse.TabIndex = 2;
+            this.BtnBrowse.Text = "Browse";
+            this.BtnBrowse.UseVisualStyleBackColor = true;
+            this.BtnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // BtnOutput
+            // 
+            this.BtnOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnOutput.Location = new System.Drawing.Point(562, 35);
+            this.BtnOutput.Name = "BtnOutput";
+            this.BtnOutput.Size = new System.Drawing.Size(75, 31);
+            this.BtnOutput.TabIndex = 3;
+            this.BtnOutput.Text = "Output";
+            this.BtnOutput.UseVisualStyleBackColor = true;
+            this.BtnOutput.Click += new System.EventHandler(this.BtnOutput_Click);
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label4.Location = new System.Drawing.Point(164, 98);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(103, 17);
+            this.Label4.TabIndex = 10;
+            this.Label4.Text = "Load a csv file:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // cbDetailedMode
+            // 
+            this.cbDetailedMode.AutoSize = true;
+            this.cbDetailedMode.Location = new System.Drawing.Point(481, 82);
+            this.cbDetailedMode.Name = "cbDetailedMode";
+            this.cbDetailedMode.Size = new System.Drawing.Size(92, 17);
+            this.cbDetailedMode.TabIndex = 7;
+            this.cbDetailedMode.Text = "detailed mode";
+            this.cbDetailedMode.UseVisualStyleBackColor = true;
+            this.cbDetailedMode.CheckedChanged += new System.EventHandler(this.cbDetailedMode_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 500);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Panel1.ResumeLayout(false);
+            this.Panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Panel Panel1;
+        internal System.Windows.Forms.TextBox TbUploadFile;
+        internal System.Windows.Forms.Button BtnBrowse;
+        internal System.Windows.Forms.Button BtnOutput;
+        internal System.Windows.Forms.Label Label4;
+        public System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cbDetailedMode;
+    }
+}
+
---
+++ Post/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Post
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+        }
+
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+
+            //Open a dialog to select a csv file
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                TbUploadFile.Text = openFileDialog1.FileName;
+        }
+
+        private void BtnOutput_Click(object sender, EventArgs e)
+        {
+            string strFileName = TbUploadFile.Text;
+            bool isDetailedMode = cbDetailedMode.Checked;
+            FileFormat(strFileName);
+
+            FileProcess(@"C:\temp.csv", isDetailedMode);
+        }
+
+        //Add a new timestamp_new column, sort by timestamp_new and likes
+        private void FileFormat(string filepath)
+        {
+            try
+            {
+                using (var reader = new StreamReader(filepath))
+                {
+                    int i = 0;
+                    int len = 0;
+
+                    var timestamp_new = "";
+
+                    StringBuilder builder = new StringBuilder();
+
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+                        len = values.Length;
+
+                        if (i == 0)
+                            builder.AppendLine(line + ",timestamp_new"); //Add column header
+                        else //Format date in timestamp column and put to the "timestamp_new" column
+                        {
+                            var timestamp = values[len - 1].ToString().Split(' ');
+
+                            timestamp_new = timestamp[1].ToString() + " " + timestamp[2].ToString() + " " + timestamp[4].ToString();
+
+                            var date_new = DateTime.Parse(timestamp_new);
+
+                            DateTime dt = new DateTime();
+                            dt = Convert.ToDateTime(date_new);
+
+                            var date = dt.ToString("yyyy-MM-dd");
+
+                            builder.AppendLine(line + "," + date.ToString());
+                        }
+                        i++;
+                    }
+
+                    //Save to temp.csv
+                    WriteToCVS(@"C:\temp.csv", builder.ToString());
+
+                    //sort temp.csv
+                    string[] lines = File.ReadAllLines(@"C:\temp.csv");
+                    var data = lines.Skip(1);
+
+                    //Sort by "timestamp_new" and "likes"
+                    var sorted = data.Select(line => new
+                    {
+                        SortKey = (line.Split(',')[line.Split(',').Length - 1]),
+                        SortKeyThenBy = Int32.Parse(line.Split(',')[line.Split(',').Length - 5]),
+                        Line = line
+                    })
+                    .OrderBy(x => x.SortKey).ThenByDescending(x => x.SortKeyThenBy)
+                    .Select(x => x.Line);
+
+                    //Save sorted data to temp.csv
+                    File.WriteAllLines(@"C:\temp.csv", lines.Take(1).Concat(sorted));
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("DateFormat function failed: " + ex.Message);
+            }
+        }
+
+        //Create the output files
+        private void FileProcess(string filepath, bool isDetailedMode)
+        {
+            try
+            {
+                //Read temp.csv
+                using (var reader = new StreamReader(filepath))
+                {
+                    var id = "";
+                    var title = "";
+                    var privacy = "";
+                    int likes = 0;
+                    int likes_highest = 0;
+                    int views = 0;
+                    int comments = 0;
+                    var timestamp = "";
+                    var timestamp_new = "";
+                    var timestamp_new_temp = "";
+                    int len = 0;
+                    int j = 0;
+                    var line_new = "";
+
+                    StringBuilder builder_top_posts = new StringBuilder();
+                    StringBuilder builder_other_posts = new StringBuilder();
+                    StringBuilder builder_daily_top_posts = new StringBuilder();
+
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+                        len = values.Length;
+
+                        //Create header
+                        if (j == 0)
+                        {
+                            line_new = values[0];
+
+                            if (isDetailedMode)
+                                line_new += "," + values[1] + "," + values[2] + "," + values[3] + "," + values[4] + "," + values[5] + "," + values[6];
+
+
+                            builder_top_posts.AppendLine(line_new);
+                            builder_other_posts.AppendLine(line_new);
+                            builder_daily_top_posts.AppendLine(line_new);
+                        }
+                        else
+                        {
+                            id = values[0].ToString();
+
+                            title = "";
+
+                            for (int i = 1; i <= len - 7; i++)
+                            {
+                                title += values[i];
+
+                                if (i != len - 7)
+                                    title += ",";
+                            }
+
+                            privacy = values[len - 6].ToString();
+                            likes = Int32.Parse(values[len - 5]);
+                            views = Int32.Parse(values[len - 4]);
+                            comments = Int32.Parse(values[len - 3]);
+                            timestamp = values[len - 2].ToString();
+                            timestamp_new = values[len - 1].ToString();
+
+                            line_new = id;
+                            if (isDetailedMode)
+                                line_new += "," + title + "," + privacy + "," + likes + "," + views + "," + comments + "," + timestamp;
+
+
+                            if (isTopPosts(privacy, comments, views, title))
+                            {
+                                builder_top_posts.AppendLine(line_new); //put top posts to builder_top_posts
+                            }
+                            else
+                            {
+                                builder_other_posts.AppendLine(line_new); //put remaining posts to builder_other_posts
+                            }
+
+                            if (timestamp_new != timestamp_new_temp) //Since the temp.csv file ordered by date and the 'likes' (descending), so the top row must be one of the top posts for the new day
+                            {
+                                builder_daily_top_posts.AppendLine(line_new); //put daily top posts to builder_daily_top_posts
+                                likes_highest = likes;
+                            }
+                            else if (likes == likes_highest)
+                                builder_daily_top_posts.AppendLine(line_new); //if found a post with the same 'likes', put daily the posts to builder_daily_top_posts
+
+                            timestamp_new_temp = timestamp_new;
+                        }
+
+                        j++;
+                    }
+
+                    //Create output files
+                    WriteToCVS(@"C:\posts_top_posts.csv", builder_top_posts.ToString());
+                    WriteToCVS(@"C:\posts_other_posts.csv", builder_other_posts.ToString());
+                    WriteToCVS(@"C:\posts_daily_posts.csv", builder_daily_top_posts.ToString());
+
+                    label1.Text = "posts_top_posts.csv is created under C drive.";
+                    label2.Text = "posts_other_posts.csv is created under C drive.";
+                    label3.Text = "posts_daily_posts.csv is created under C drive.";
+                }
+
+                //Delete the temp.csv if exists
+                if (File.Exists(@"C:\temp.csv"))
+                {
+                    File.Delete(@"C:\temp.csv");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("FileProcess function failed: " + ex.Message);
+            }
+        }
+
+        private bool isTopPosts(string privacy, int comments, int views, string title)
+        {
+            if (privacy.ToLower() == "public" && comments > 10 && views > 9000 && title.Replace("\"\"", " ").Replace("\"", "").Length < 40)
+                return true;
+            else
+                return false;
+        }
+
+        private void WriteToCVS(string filepath, string input)
+        {
+            File.WriteAllText(filepath, input);
+        }
+
+        private void cbDetailedMode_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+        }
+    }
+}
